61788412a103d601524a6309453e638420b91ec1
dkvir <kvirika137@gmail.com>
2023-12-24 16:13:42 +0400

    👌 IMPROVE: texture loader

--- src/js/app.js
+++ src/js/app.js
@@ -2,55 +2,8 @@ import * as PIXI from 'pixi.js';
 import { gsap } from "gsap";
 import { Sprite, Texture } from 'pixi.js';
 
-//global variables
-let bgBoxes = document.querySelectorAll('.change-bg .box');
-const wheelSectors = [
-  {
-    text: "2M",
-    prize: "2M",
-    color: 0xf9b807,
-  },
-  {
-    icon: "/images/icons/iron.png",
-    prize: "Iron",
-    color: 0x25d22,
-  },
-  {
-    text: "3M",
-    prize: "3M",
-    color: 0xe54746,
-  },
-  {
-    icon: "/images/icons/sedan.png",
-    prize: "Car",
-    color: 0x269271,
-  },
-  {
-    text: "1M",
-    prize: "1M",
-    color: 0x376fa3,
-  },
-  {
-    icon: "/images/icons/home.png",
-    prize: "Home",
-    color: 0x9a1c4b,
-  },
-  {
-    text: "5M",
-    prize: "5M",
-    color: 0xd6d4c6,
-  },
-  {
-    icon: "/images/icons/motorcycle.png",
-    prize: "Bike",
-    color: 0x676854,
-  },
-]
-const sectorCount = wheelSectors.length;
-let animating = false;
-
 //load PIXI
-let app = new PIXI.Application({
+const app = new PIXI.Application({
   width: window.innerWidth,
   height:window.innerHeight,
   resizeTo: window,
@@ -60,108 +13,172 @@ let app = new PIXI.Application({
 })
 
 app.renderer.view.style.position = 'absolute'
+const loader = PIXI.Loader.shared;
 
-// change background textures
-changeBgTexture();
+loader.add('backgroundGreen', '/images/background-1.avif')
+.add('backgroundBlue', '/images/background-2.avif')
+.add('backgroundRed', '/images/background-3.avif')
+.add('iron', '/images/icons/iron.png')
+.add('home', '/images/icons/home.png')
+.add('bike', '/images/icons/bike.png')
+.add('car', '/images/icons/car.png')
+.load(setup)
 
-
-//background texture
-const bgTextures = [Texture.from('/images/background-1.avif'), Texture.from('/images/background-2.avif'), Texture.from('/images/background-3.avif')]
-const background = new Sprite(bgTextures[0]);
-background.width = window.innerWidth;
-background.height = window.innerHeight;
-app.stage.addChild(background);
-
-
-//wheel container
+//containers
 const wheel = new PIXI.Container();
-const circleDeg =  Math.PI * 2;
-const oneSector = circleDeg / sectorCount;
-let radius = window.innerWidth / 6;
-const centerX = window.innerWidth / 2;
-const centerY = window.innerHeight / 2 - radius / 3;
-wheel.position.set(centerX, centerY);
-app.stage.addChild(wheel);
-
-//sector container
 const sectorContainer = new PIXI.Container();
 
-// wheel graphics
-const wheelGraphicsBg = new PIXI.Graphics();
-wheelGraphicsBg.beginFill(0x1f2f32).arc(0, 0, radius, 0, 360);
-wheel.addChild(wheelGraphicsBg);
-
-wheelSectors.forEach((item, i)=>{
-  const startAngle = (i * circleDeg) / sectorCount - 5 * (oneSector /2);
-  const endAngle = ((i + 1) * circleDeg) / sectorCount - 5 * (oneSector /2);
-
-  // Create sector
-  const sector = createSector(startAngle, endAngle, item);
-  wheel.addChild(sector);
-})
-
-//button
+//global variables
+let wheelSectors, sectorCount, bgBoxes, oneSector, background, bgTextures;
+let radius = window.innerWidth / 6;
 const buttonRadius = 70;
-const button = new PIXI.Graphics();
-button.beginFill(0xcf0300)
-.drawCircle(0,0, buttonRadius)
-.endFill();
-button.interactive = true;
-button.buttonMode = true;
-button.cursor = 'pointer';
-wheel.addChild(button);
+const circleDeg =  Math.PI * 2;
+const centerX = window.innerWidth / 2;
+const centerY = window.innerHeight / 2 - radius / 3;
+let animating = false;
 
-const triangleGraphics = new PIXI.Graphics();
-triangleGraphics.beginFill(0xcf0300); // Change the color as needed
-triangleGraphics.moveTo(-15, -buttonRadius - 5);
-triangleGraphics.lineTo(20, -buttonRadius - 5);
-triangleGraphics.lineTo(0, -buttonRadius - 30);
-triangleGraphics.lineTo(-20, -buttonRadius - 5);
-triangleGraphics.endFill();
-button.addChild(triangleGraphics);
 
-const buttonText = new PIXI.Text('SPIN', { fill: 0xf3f6f4, fontSize: 35 });
-buttonText.anchor.set(0.5, 0.5);
-button.addChild(buttonText);
+function setup(loader, resources){
 
-//event listeners
-// Change button color on mouseover
-button.on('pointerover', () => {
-  button.clear();
-  button.beginFill(0x1f2f32); // New color on mouseover, change as needed
-  button.drawCircle(0, 0, buttonRadius);
-  button.endFill();
+  wheelSectors = [
+    {
+      text: "2M",
+      prize: "2M",
+      color: 0xf9b807,
+    },
+    {
+      icon: resources.iron.texture,
+      prize: "Iron",
+      color: 0x25d22,
+    },
+    {
+      text: "3M",
+      prize: "3M",
+      color: 0xe54746,
+    },
+    {
+      icon: resources.car.texture,
+      prize: "Car",
+      color: 0x269271,
+    },
+    {
+      text: "1M",
+      prize: "1M",
+      color: 0x376fa3,
+    },
+    {
+      icon: resources.home.texture,
+      prize: "Home",
+      color: 0x9a1c4b,
+    },
+    {
+      text: "5M",
+      prize: "5M",
+      color: 0xd6d4c6,
+    },
+    {
+      icon: resources.bike.texture,
+      prize: "Bike",
+      color: 0x676854,
+    },
+  ]
+  sectorCount = wheelSectors.length;
+  bgBoxes = document.querySelectorAll('.change-bg .box')
+  oneSector = circleDeg / sectorCount;
 
-  triangleGraphics.clear();
-  triangleGraphics.beginFill(0x1f2f32);
+  //background texture
+  bgTextures = [resources.backgroundGreen, resources.backgroundBlue, resources.backgroundRed]
+  background = new Sprite(bgTextures[0].texture);
+  background.width = window.innerWidth;
+  background.height = window.innerHeight;
+  app.stage.addChild(background);
+
+  // change background textures
+  changeBgTexture(background);
+
+
+  //wheel container
+  wheel.position.set(centerX, centerY);
+  app.stage.addChild(wheel);
+
+
+  // wheel graphics
+  const wheelGraphicsBg = new PIXI.Graphics();
+  wheelGraphicsBg.beginFill(0x1f2f32).arc(0, 0, radius, 0, 360);
+  wheel.addChild(wheelGraphicsBg);
+
+  wheelSectors.forEach((item, i)=>{
+    const startAngle = (i * circleDeg) / sectorCount - 5 * (oneSector /2);
+    const endAngle = ((i + 1) * circleDeg) / sectorCount - 5 * (oneSector /2);
+
+    // Create sector
+    const sector = createSector(startAngle, endAngle, item);
+    wheel.addChild(sector);
+  })
+
+  //button
+  const button = new PIXI.Graphics();
+  button.beginFill(0xcf0300)
+  .drawCircle(0,0, buttonRadius)
+  .endFill();
+  button.interactive = true;
+  button.buttonMode = true;
+  button.cursor = 'pointer';
+  wheel.addChild(button);
+
+  const triangleGraphics = new PIXI.Graphics();
+  triangleGraphics.beginFill(0xcf0300); // Change the color as needed
   triangleGraphics.moveTo(-15, -buttonRadius - 5);
   triangleGraphics.lineTo(20, -buttonRadius - 5);
   triangleGraphics.lineTo(0, -buttonRadius - 30);
   triangleGraphics.lineTo(-20, -buttonRadius - 5);
   triangleGraphics.endFill();
-});
+  button.addChild(triangleGraphics);
 
-// Restore original button color on mouseout
-button.on('pointerout', () => {
-  button.clear();
-  button.beginFill(0xcf0300); // Original color on mouseout, change as needed
-  button.drawCircle(0, 0, buttonRadius);
-  button.endFill();
+  const buttonText = new PIXI.Text('SPIN', { fill: 0xf3f6f4, fontSize: 35 });
+  buttonText.anchor.set(0.5, 0.5);
+  button.addChild(buttonText);
 
-  triangleGraphics.clear();
-  triangleGraphics.beginFill(0xcf0300);
-  triangleGraphics.moveTo(-15, -buttonRadius - 5);
-  triangleGraphics.lineTo(20, -buttonRadius - 5);
-  triangleGraphics.lineTo(0, -buttonRadius - 30);
-  triangleGraphics.lineTo(-20, -buttonRadius - 5);
-  triangleGraphics.endFill();
-});
+  //event listeners
+  // Change button color on mouseover
+  button.on('pointerover', () => {
+    button.clear();
+    button.beginFill(0x1f2f32); // New color on mouseover, change as needed
+    button.drawCircle(0, 0, buttonRadius);
+    button.endFill();
 
-button.on('pointerdown', onButtonClick);
-window.addEventListener('resize', resize);
+    triangleGraphics.clear();
+    triangleGraphics.beginFill(0x1f2f32);
+    triangleGraphics.moveTo(-15, -buttonRadius - 5);
+    triangleGraphics.lineTo(20, -buttonRadius - 5);
+    triangleGraphics.lineTo(0, -buttonRadius - 30);
+    triangleGraphics.lineTo(-20, -buttonRadius - 5);
+    triangleGraphics.endFill();
+  });
+
+  // Restore original button color on mouseout
+  button.on('pointerout', () => {
+    button.clear();
+    button.beginFill(0xcf0300); // Original color on mouseout, change as needed
+    button.drawCircle(0, 0, buttonRadius);
+    button.endFill();
+
+    triangleGraphics.clear();
+    triangleGraphics.beginFill(0xcf0300);
+    triangleGraphics.moveTo(-15, -buttonRadius - 5);
+    triangleGraphics.lineTo(20, -buttonRadius - 5);
+    triangleGraphics.lineTo(0, -buttonRadius - 30);
+    triangleGraphics.lineTo(-20, -buttonRadius - 5);
+    triangleGraphics.endFill();
+  });
+
+  button.on('pointerdown',()=> onButtonClick(buttonText));
+  window.addEventListener('resize', resize);
+
+}
 
 
-function onButtonClick() {
+function onButtonClick(buttonText) {
   if(!animating){
     animating = true
     let stopIndex = getRandomInt(0,sectorCount - 1);
@@ -186,10 +203,10 @@ function onButtonClick() {
 }
 
 //functions
-function changeBgTexture(){
+function changeBgTexture(background){
   bgBoxes.forEach((box,index)=>{
     box.addEventListener('click', function(){
-      background.texture = bgTextures[index];
+      background.texture = bgTextures[index].texture;
     })
   })
 }
@@ -229,8 +246,7 @@ function createSector(startAngle, endAngle, item) {
     text.position.set(centerCoordinates.x, centerCoordinates.y);
     sectorContainer.addChild(text);
   } else {
-    const texture = PIXI.Texture.from(item.icon); 
-    const sprite = new PIXI.Sprite(texture);
+    const sprite = new PIXI.Sprite(item.icon);
     sprite.anchor.set(0.5, 0.5);
     sprite.rotation = rotationAngle;
     sprite.position.set(centerCoordinates.x, centerCoordinates.y);
